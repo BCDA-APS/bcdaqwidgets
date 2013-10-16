88e8303041f229356840cdab7f2b2dccac515939
Pete R Jemian <prjemian@users.noreply.github.com>
2013-10-16 14:24:17 +0000

    minor

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ from setuptools import setup, find_packages
 
 import os, sys
 
-#sys.path.insert(0, os.path.abspath(os.path.join('src', 'APSpy')))
 sys.path.insert(0, os.path.join('src', ))
 import bcdaqwidgets
 
